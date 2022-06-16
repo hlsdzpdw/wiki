f20d76e0589eae994a5f2f7c67a969b0291de7af
liu <1009263692@qq.com>
2022-06-16 20:54:25 +0800

    4.8 Vue多环境配置，增加dev和prod配置文件 编译和启动命令增加多环境

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,3 +6,6 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
 createApp(App).use(store).use(Antd).use(router).mount('#app');
+
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_SERVER);
